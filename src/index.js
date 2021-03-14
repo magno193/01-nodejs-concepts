7ba68ed412d8fefeee13504a7ac6eaedbfaabe1a
Alexandre Ferreira <alexmlf93@gmail.com>
2021-03-14 04:14:20 -0300

    User tests complete

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request;
+  if (!username) {
+    return response.status(400).json({ error: 'Invalid username' });
+  }
+  next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  const findUser = users.some(user => user.username === username);
+  if (findUser) {
+    return response.status(400).json({ error: 'User already exists' });
+  }
+  request.username = username;
+  const user = { id: uuid(), name, username, todos: [] }
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
